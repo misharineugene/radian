163810543de7bc52baeca51347debb4046b5eae7
misharineugene <misharin.eugene@gmail.com>
2020-12-04 01:58:19 +0300

    Finished site header

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,26 @@
-// import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
+import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
 // require('./other_script.js') // Require Other Script(s) from app/js folder Example
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+  const mobileToggle = document.querySelector('.g-header__toggle')
+  const mobileNav = document.querySelector('.g-header__mobile')
+  const body = document.body
+  mobileToggle.addEventListener('click', () => {
+    mobileToggle.classList.toggle('open')
+    mobileNav.classList.toggle('open')
+    body.classList.toggle('open')
+  })
+
+  const header 		= $('#g-header'),
+        topHeight = header.outerHeight()
+         
+  $(window).on('load scroll', function() {
+    if ( !header.hasClass('scroll') && $(this).scrollTop() > topHeight ) {
+      header.addClass('scroll');
+    } else if ( header.hasClass('scroll') && $(this).scrollTop() <= topHeight ) {
+      header.removeClass('scroll');
+    }
+  })
 
 })
